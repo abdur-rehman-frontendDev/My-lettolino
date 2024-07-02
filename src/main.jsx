c87406875bc53fe2be978ef061bdfcf3f38f90f9
Abdur Rehman <ab.rehman2534@gmail.com>
2024-07-02 13:14:37 +0500

    bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/My-lettolino">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/My-lettolino',
+  base: '/My-lettolino/',
   plugins: [react()],
   server: {
     port: 4000,
